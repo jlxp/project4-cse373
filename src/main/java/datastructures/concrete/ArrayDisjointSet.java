3914cb71e06da2d7a973f8f8c92d81f0b7b7e0b2
Joy Phillips <jlxp@uw.edu>
2018-05-26 11:02:34 -0700

    working on DisjointSet

--- src/main/java/datastructures/concrete/ArrayDisjointSet.java
+++ src/main/java/datastructures/concrete/ArrayDisjointSet.java
@@ -1,5 +1,7 @@
 package datastructures.concrete;
 
+import datastructures.concrete.dictionaries.ChainedHashDictionary;
+import datastructures.interfaces.IDictionary;
 import datastructures.interfaces.IDisjointSet;
 import misc.exceptions.NotYetImplementedException;
 
@@ -10,27 +12,72 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
     // Note: do NOT rename or delete this field. We will be inspecting it
     // directly within our private tests.
     private int[] pointers;
-
+    private static final int INIT_LENGTH = 10;
+    private IDictionary<T, Integer> data;
+    private int size;
     // However, feel free to add more methods and private helper methods.
     // You will probably need to add one or two more fields in order to
     // successfully implement this class.
 
     public ArrayDisjointSet() {
-        // TODO: your code here
+        pointers = new int[INIT_LENGTH];
+        data = new ChainedHashDictionary<T, Integer>();
+        size = 0;
     }
 
     @Override
     public void makeSet(T item) {
-        throw new NotYetImplementedException();
+        if (this.contains(item)) {
+            throw new IllegalArgumentException("Already in the Set");
+        }
+        if (this.pointers.length == this.size) {
+            int[] temp = new int[this.pointers.length * 2];
+            for (int i = 0; i < this.pointers.length; i++) {
+                temp[i] = this.pointers[i];
+            }
+            this.pointers = temp;
+        }
+        this.data.put(item, this.size);
+        this.pointers[this.size] = -1;
+        this.size++;
     }
 
     @Override
     public int findSet(T item) {
-        throw new NotYetImplementedException();
+        if (this.contains(item)) {
+            throw new IllegalArgumentException("Already in the Set");
+        }
+        return this.data.get(item);
     }
 
+    private boolean contains(T item) {
+        return this.data.containsKey(item);
+    }
+    
     @Override
     public void union(T item1, T item2) {
-        throw new NotYetImplementedException();
+        if (this.contains(item1) || this.contains(item2)) {
+            throw new IllegalArgumentException("Already in the set");
+        }
+        if (this.isInSameSet(item1, item2)) {
+            throw new IllegalArgumentException("in the same set");
+        }
+        
+    }
+    
+    private boolean isInSameSet(T item1, T item2) {
+        int index1 = this.data.get(item1);
+        int index2 = this.data.get(item2);
+        boolean found = false;
+        while (!found) {
+            if (index1 >= 0) {
+                index1 = this.pointers[index1];
+            }
+            if (index2 >= 0) {
+                index2 = this.pointers[index2];
+            }
+            found = index1 < 0 && index2 < 0;
+        }
+        return index1 == index2;
     }
 }
