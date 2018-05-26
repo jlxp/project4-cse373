a825d255c1ef5d52e511ac1c01bcb0f4ce7d48b9
warandstar <jtk7@uw.edu>
2018-05-26 11:57:56 -0700

    finished ds

--- src/main/java/datastructures/concrete/ArrayDisjointSet.java
+++ src/main/java/datastructures/concrete/ArrayDisjointSet.java
@@ -44,10 +44,20 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
 
     @Override
     public int findSet(T item) {
-        if (this.contains(item)) {
-            throw new IllegalArgumentException("Already in the Set");
+        if (!this.contains(item)) {
+            throw new IllegalArgumentException("Not in the Set");
         }
-        return this.data.get(item);
+        int index = this.data.get(item);        
+        return this.findRank(index);
+    }
+    
+    private int findRank(int index) {
+        if (this.pointers[index] < 0) {
+            return index;
+        }
+        int result = this.findRank(this.pointers[index]);
+        this.pointers[index] = result;
+        return result;
     }
 
     private boolean contains(T item) {
@@ -56,47 +66,28 @@ public class ArrayDisjointSet<T> implements IDisjointSet<T> {
     
     @Override
     public void union(T item1, T item2) {
-        if (this.contains(item1) || this.contains(item2)) {
-            throw new IllegalArgumentException("Already in the set");
+        if (!this.contains(item1) || !this.contains(item2)) {
+            throw new IllegalArgumentException("Not in the set");
         }
-        if (this.isInSameSet(item1, item2)) {
+        int index1 = this.findSet(item1);
+        int index2 = this.findSet(item2); 
+        if (index1 == index2) {
             throw new IllegalArgumentException("in the same set");
         }
         // get the index from data of item1 and item2; then check in the pointers;
         // returns neg number; (rank * -1) -1
-        int index1 = this.data.get(item1);
-        int index2 = this.data.get(item2);
-        boolean found = false;
-        while (!found) {
-            if (index1 >= 0) {
-                index1 = this.pointers[index1];
-            }
-            if (index2 >= 0) {
-                index2 = this.pointers[index2];
-            }
-            found = index1 < 0 && index2 < 0;
-        }
-        if (index1 > index2) {
-            index2 = index1;
+        
+        int rank1 = this.pointers[index1];
+        int rank2 = this.pointers[index2];
+        if (rank2 < rank1) { // rank2 is actually larger than rank1!!!!!!!
+            this.pointers[index1] = index2;
+            
+        } else if (rank2 > rank1){
+            this.pointers[index2] = index1;
         } else {
-            index1 = index2;
+            this.pointers[index2] = index1;
+            this.pointers[index1]--;
         }
-        index
-    }
-    
-    private boolean isInSameSet(T item1, T item2) {
-        int index1 = this.data.get(item1);
-        int index2 = this.data.get(item2);
-        boolean found = false;
-        while (!found) {
-            if (index1 >= 0) {
-                index1 = this.pointers[index1];
-            }
-            if (index2 >= 0) {
-                index2 = this.pointers[index2];
-            }
-            found = index1 < 0 && index2 < 0;
-        }
-        return index1 == index2;
+                
     }
 }
